9a7c21808c6bbed975118a0ba5c7c31ee1388b05
Mithunchowdhury <mithuncste@gmail.com>
2015-05-14 07:12:44 +0600

    you teacharApp

---
+++ TeacherApp/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace TeacherApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.AddressTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.showListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.showButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(101, 32);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 3;
+            // 
+            // AddressTextBox
+            // 
+            this.AddressTextBox.Location = new System.Drawing.Point(101, 86);
+            this.AddressTextBox.Name = "AddressTextBox";
+            this.AddressTextBox.Size = new System.Drawing.Size(100, 20);
+            this.AddressTextBox.TabIndex = 4;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(101, 131);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(100, 20);
+            this.emailTextBox.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Salary";
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Location = new System.Drawing.Point(101, 171);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(100, 20);
+            this.salaryTextBox.TabIndex = 7;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(111, 216);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // showListView
+            // 
+            this.showListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.showListView.GridLines = true;
+            this.showListView.Location = new System.Drawing.Point(27, 277);
+            this.showListView.Name = "showListView";
+            this.showListView.Size = new System.Drawing.Size(376, 183);
+            this.showListView.TabIndex = 9;
+            this.showListView.UseCompatibleStateImageBehavior = false;
+            this.showListView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Address";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Email";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Salary";
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(246, 215);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 10;
+            this.showButton.Text = "showButton";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 472);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.showListView);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.salaryTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.AddressTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "TeacherUI";
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox AddressTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.ListView showListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button showButton;
+    }
+}
+
---
+++ TeacherApp/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeacherApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            string name = nameTextBox.Text;
+            string address = AddressTextBox.Text;
+            string email = emailTextBox.Text;
+            decimal salary = Convert.ToDecimal(salaryTextBox.Text);
+
+            if (IsEmailExists(email))
+            {
+
+                MessageBox.Show("email   already  exits");
+            }
+
+            string connectionstring = "SERVER= MITHUN-PC; Database=MithunInfoDB;Integrated security=true";
+
+
+            SqlConnection con = new SqlConnection(connectionstring);
+
+            string query = "INSERT INTO mithuns VALUES('" + name + "','" + address + "','" + email + "','" + salary +
+                           "') ";
+            SqlCommand command = new SqlCommand(query, con);
+
+            con.Open();
+            int Rowaffected = command.ExecuteNonQuery();
+            con.Close();
+            if (Rowaffected > 0)
+            {
+
+                MessageBox.Show("Inserted sucessfully");
+            }
+            else
+            {
+                MessageBox.Show("Not inserted");
+            }
+            //connection datebasae
+        }
+
+        public bool IsEmailExists(string email)
+        {
+            string connectionstring = "SERVER= MITHUN-PC; Database=MithunInfoDB;Integrated security=true";
+
+
+            SqlConnection con = new SqlConnection(connectionstring);
+
+            string query = "SELECT * FROM mithuns WHERE Email='" + email + "'";
+
+            SqlCommand command = new SqlCommand(query, con);
+            bool isRegNOExists = false;
+            con.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                isRegNOExists = true;
+                break;
+
+
+            }
+            con.Close();
+            reader.Close();
+            return isRegNOExists;
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            string connectionstring =  "SERVER=MITHUN-PC; Database=MithunInfoDB;Integrated security=true";
+            SqlConnection con = new SqlConnection(connectionstring);
+            string query = "SELECT * FROM  mithuns";
+            SqlCommand command = new SqlCommand(query, con);
+
+            con.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            List<Teacher> teacherList = new List<Teacher>();
+            while (reader.Read())
+            {
+                Teacher aTeacher = new Teacher();
+                aTeacher.id = int.Parse(reader["ID"].ToString());
+                aTeacher.name = reader["Name"].ToString();
+                aTeacher.address = reader["Address"].ToString();
+                aTeacher.email = reader["Email"].ToString();
+                aTeacher.salary = Convert.ToDecimal(reader["Salary"].ToString());
+
+                teacherList.Add(aTeacher);
+            }
+            con.Close();
+            reader.Close();
+            LoadStudentListView(teacherList);
+
+        }
+
+        public void LoadStudentListView(List<Teacher> teachers)
+        {
+
+            foreach (var teacher in teachers)
+            {
+                ListViewItem item = new ListViewItem(teacher.id.ToString());
+
+                item.SubItems.Add(teacher.name);
+                item.SubItems.Add(teacher.address);
+                item.SubItems.Add(teacher.email);
+                item.SubItems.Add(teacher.salary.ToString());
+                showListView.Items.Add(item);
+            }
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            ListViewItem item=new ListViewItem();
+            int id = int.Parse(item.Text.ToString());
+             Teacher aTeacher=
+
+
+
+        }
+
+        public Student GetStudentBy( int ID )
+        {
+            
+
+
+        }
+          
+
+
+      
+    }
+}
+    
+
